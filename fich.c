03e0425bcc9023cd62fad57b578d7f9e7788878e
jm2meulien <jm2meulien@hotmail.fr>
2018-05-01 14:43:29 +0200

    Create fich.c

---
+++ fich.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+printf("Bonjour à tous\n");
+int nombre= 5;
+printf("Vous avez un nombre de %d \n", nombre);
+nombre = 4;
+printf("Maintenant, vous avez un nombre de %d \n\n", nombre);
+return 0;
+}
